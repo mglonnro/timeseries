732cf72ffa4cdb743dc334fd5efd7f9a0d025df8
Mikael Lönnroth <mikael@humeko.com>
2020-12-09 18:34:58 +0200

    Upgraded to mjs.

--- index.js
+++
@@ -1,5 +0,0 @@
-const TimeData = require("./TimeData");
-const TimeRange = require("./TimeRange");
-const TimeSeries = require("./TimeSeries");
-
-module.exports = { TimeData, TimeRange, TimeSeries };
---
+++ index.mjs
@@ -0,0 +1,5 @@
+import TimeData from "./TimeData";
+import TimeRange from "./TimeRange";
+import TimeSeries from "./TimeSeries";
+
+export { TimeData, TimeRange, TimeSeries };
