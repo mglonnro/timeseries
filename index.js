ae6fd22bb3b59f38931c3b495d8ff78f645ddb6a
Mikael Lönnroth <mikael@humeko.com>
2022-09-17 20:12:28 +0300

    v0.0.5

---
+++ index.js
@@ -0,0 +1,5 @@
+import TimeData from "./TimeData.js";
+import TimeRange from "./TimeRange.js";
+import TimeSeries from "./TimeSeries.js";
+
+export { TimeData, TimeRange, TimeSeries };
--- index.mjs
+++
@@ -1,5 +0,0 @@
-import TimeData from "./TimeData.mjs";
-import TimeRange from "./TimeRange.mjs";
-import TimeSeries from "./TimeSeries.mjs";
-
-export { TimeData, TimeRange, TimeSeries };
